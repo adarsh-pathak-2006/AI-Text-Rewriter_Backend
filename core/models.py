f4e38f0573b45848379a5183b9345019c1bd894c
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-07 11:28:14 +0530

    configured models

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class MainDB(models.Model):
     input=models.TextField()
     style=models.CharField(max_length=15, choices=[('PROFESSIONAL','Professional'), ('CASUAL', 'Casual'), ('RESPECT-TONED', 'Respect-Toned')])
     output=models.TextField(null=True)
+    time=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.input[:100]
